9b263b25685a47801f15f23d58e9953668b87da0
Bryan Neva <bneva@pivotal.io>
2019-11-22 16:24:38 -0500

    Update tests, add mocha script, add semicolons

--- main.test.ts
+++ main.test.ts
@@ -1,17 +1,26 @@
-import {PeopleSource, SlackClient, StandupMaster} from './main'
-import * as sinon from 'sinon'
+import {PeopleDatabase, SlackClient, StandupMaster} from "./main";
+import sinon = require("sinon");
 
 describe('StandupMaster', () => {
     describe('#chooseAndNotifyStandupRunners', () => {
         it('picks masters based on how frequently they have been picked', () => {
+            // const slack: SlackClient = {
+            //     postMessage: sinon.spy()
+            // };
+
             const slack: SlackClient = {
-                postMessage: sinon.spy()
-            }
-            const people: PeopleSource = {
-                listPeople: () => []
-            }
-            const standupMaster = new StandupMaster(slack, people)
-            standupMaster.chooseAndNotifyStandupRunners()
-        })
-    })
-})
+                postMessage: sinon.spy(),
+                getUserIdByEmail: sinon.spy()
+            };
+
+            const people: PeopleDatabase = {
+                listPeople: () => [],
+                pick: sinon.spy()
+            };
+
+            const standupMaster = new StandupMaster(slack, people);
+
+            standupMaster.assignStandup();
+        });
+    });
+});
